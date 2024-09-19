6a5981d7ad5bc28383c6b7b5aeca6caa56c64adc
Abdelrahman Elrayes <abdelrahmanelrayes2@gmail.com>
2024-09-19 18:56:42 +0300

    adding map to the tour profile

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,2 +1,25 @@
 'strict mode';
-console.log('hello from home');
+
+let mapty = document.getElementById('map');
+let locations = JSON.parse(mapty.dataset.locations);
+
+
+
+
+let firstLocation = locations[0].coordinates;
+var map = L.map('map').setView(firstLocation, 3);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+locations.forEach((loc) => {
+  L.marker(loc.coordinates)
+    .addTo(map)
+    .bindPopup(loc.description, {
+      autoClose: false,
+      closeOnClick: false,
+    })
+    .openPopup();
+});
